f63bedaaa5664f71ed0a26bec526ffb67143fc0b
hayder <hayderbd4290@gmail.com>
2024-08-29 22:25:34 +0600

    Frist commit setup done

---
+++ script.js
@@ -0,0 +1,18 @@
+console.clear();
+let p1 = document.querySelector("#p1");
+
+
+let btn1 = document.querySelector("#btn1");
+btn1.addEventListener("click", function (){
+    window.location.reload();
+});
+
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+
